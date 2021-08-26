7adebdf2fb3ba3df91516fb6e9b2fc908af4c0d2
EmilyDSarani <emily.sarani@gmail.com>
2021-08-26 12:55:08 -0500

    learning to test Addcart

--- test/render.test.js
+++ test/render.test.js
@@ -17,3 +17,43 @@
     // Make assertions about what is expected versus the actual result
     //expect.equal(actual.outerHTML, expected);
 //});
+
+//Dani Went over this one in class
+import { addTooCart } from '../local-storage-utils.js';
+
+const test = QUnit.test;
+
+test('addtooCart take in id and icrement quantity, if it exists locally', (expect) => {
+    
+    const cartBefore = [
+        { id: 1,
+            quantity: 2
+  
+        },
+        { id: 2,
+            quantity: 1
+      
+        },
+      
+    ];
+    
+    const cartAfter = [{ id: 1,
+        quantity: 2
+
+    },
+    { id: 2,
+        quantity: 4
+
+    },
+
+    ];
+
+
+    const stringyCart = JSON.stringify(cartBefore);
+    localStorage.setItem ('CART', stringyCart);
+    addTooCart (2, 1);
+    const actual = JSON.parse (localStorage.getItem('CART'));
+
+    expect.deepEqual(actual, cartAfter);
+});
+
